1db6f53f183b166c60e7f3cdab9ffdaf4b878a19
zhenglunsen <zhenglunsen@ioive.com>
2019-06-05 16:15:43 +0800

    20190605-bug-2

--- app/Http/Controllers/Sys/UserController.php
+++ app/Http/Controllers/Sys/UserController.php
@@ -316,7 +316,7 @@ class UserController extends Controller
             $this->error_message('手机号已存在');
         }
         $email = User::where('email',$data['email'])->where('status','!=',-1)->first();
-        if($email && $phone->id != $user->id)
+        if($email && $email->id != $user->id)
         {
             $this->error_message('邮箱已存在');
         }
